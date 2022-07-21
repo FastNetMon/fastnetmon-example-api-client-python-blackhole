33dc411603ce60d5244e787937dec898d605d4d2
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-07-21 20:30:27 +0100

    Converted script to Python 3

--- fastnetmon_api_client.py
+++ fastnetmon_api_client.py
@@ -4,7 +4,7 @@ import requests
 import os
 import sys
 import time
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import random
  
 auth_data = ('admin', 'your_password_replace_it')
@@ -12,26 +12,26 @@ auth_data = ('admin', 'your_password_replace_it')
 # Create random IP tests
 ip_to_blackhole = "127.0.0.%d" % random.randint(1,254)
  
-print "Blackhole IP:", ip_to_blackhole
+print("Blackhole IP:", ip_to_blackhole)
 
 # Encode any values which have spaces or '/' inside them
-ip_to_blackhole = urllib.quote_plus(ip_to_blackhole)
+ip_to_blackhole = urllib.parse.quote_plus(ip_to_blackhole)
 
 r = requests.put('http://127.0.0.1:10007/blackhole/' + ip_to_blackhole, auth=auth_data)
  
 if r.status_code == 200:
-    print "Correctly blackholed IP"
+    print("Correctly blackholed IP")
 else:
-    print "Can't add new subnet", r.json()['error_text']
+    print("Can't add new subnet", r.json()['error_text'])
 
-print "Wait 5 second to propagate changes"
+print("Wait 5 second to propagate changes")
 time.sleep(5)
 
 # Show all blocked hosts
 r = requests.get('http://127.0.0.1:10007/blackhole', auth=auth_data)
  
 if r.status_code != 200:
-    print "API call failed", r.json()["error_text"]
+    print("API call failed", r.json()["error_text"])
     sys.exit(0)
  
 # Let's find UUID for this IP address
@@ -44,17 +44,17 @@ for host in blocked_hosts:
         blackhole_uuid = host["uuid"] 
 
 if blackhole_uuid == "":
-    print "Can't find UUID for blackholed host"
+    print("Can't find UUID for blackholed host")
     sys.exit(0)
 else:
-    print "Correctly found UUID for this IP", blackhole_uuid
+    print("Correctly found UUID for this IP", blackhole_uuid)
 
 
 r = requests.delete('http://127.0.0.1:10007/blackhole/' + blackhole_uuid, auth=auth_data)
  
 if r.status_code != 200:
-    print "Cannot delete blackhole announce"
+    print("Cannot delete blackhole announce")
     sys.exit(0)
 else:
-    print "Correctly removed blackhole announce"
+    print("Correctly removed blackhole announce")
     sys.exit(0)
